451ccc38ef3ef000654c3e7b69cdcd69488f25e5
etefaghian <etefaghian@yahoo.com>
2019-02-15 19:24:34 +0330

    im soory

--- pet-clinic-web/src/main/java/ir/etefaghian/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ir/etefaghian/sfgpetclinic/controllers/OwnerController.java
@@ -1,9 +1,28 @@
 package ir.etefaghian.sfgpetclinic.controllers;
 
+import ir.etefaghian.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.jws.WebParam;
+
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService  ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"","/","/index","index.html"})
+    public String listOwners(Model model)
+    {
+       model.addAttribute("owners",ownerService.findAll());
+        return "owners/index";
+    }
 
 
 
